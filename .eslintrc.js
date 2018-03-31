ae53769719d91b24d76388146c62ca4b8bab4ac7
tmkuba <tmk.rss@gmail.com>
2018-03-31 11:39:03 -0700

    Basic linter setup

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "eslint-config-airbnb",
+    "env": {
+      "jest": true,
+      "browser": true,
+      "node": true
+    },
+    "rules": {
+      "no-await-in-loop": 1,
+    },
+};
