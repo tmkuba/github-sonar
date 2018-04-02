ea2144f60ddef525bc5ea6618c6c7a95fb165937
tmkuba <tmk.rss@gmail.com>
2018-04-01 20:11:43 -0700

    update AJAX URL

--- client/src/ajax.js
+++ client/src/ajax.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const URL = process.env.AJAX_URL || 'http://localhost:5000';
+// const URL = process.env.AJAX_URL || 'http://localhost:5000';
 
-const search = searchTerm => axios.get(`${URL}/locations/${searchTerm}`)
+const search = searchTerm => axios.get(`/locations/${searchTerm}`)
   .then((response) => {
     // console.log('AJAX success!', response);
 
